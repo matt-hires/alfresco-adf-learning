4fcbd77d180032d07add60aafbfe761d9eac636b
matt-hires <23478257+matt-hires@users.noreply.github.com>
2024-11-22 10:24:27 +0100

    chore: fix proxy conf and edit logging

--- alfresco-adf-acs-showcase/proxy.conf.js
+++ alfresco-adf-acs-showcase/proxy.conf.js
@@ -1,7 +1,26 @@
+const { env } = require('process');
+var winston = require('winston');
+
+function logProvider() {
+    return winston.createLogger({
+        level: 'debug',
+        format: winston.format.combine(
+            winston.format.splat(),
+            winston.format.simple()
+        ),
+        transports: [new winston.transports.Console()],
+    });
+}
+
 module.exports = {
     "/alfresco": {
-        target: "http://alfresco-adf-showcase:8080",
+        target: env.HOST_NAME,
         secure: false,
-        changeOrigin: false,
+        changeOrigin: true,
+        bypass: function (req, res, proxyOptions) {
+            req.headers["origin"] = proxyOptions.target;
+        },
+        logLevel: 'debug',
+        logProvider: logProvider
     },
 };
